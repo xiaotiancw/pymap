c9b0d7a36acaa5197d3d973d2c08d866cd64b2fc
xiao Tian <waynet@163.com>
2019-06-13 23:40:25 +0800

    1

--- bmap/admin.py
+++ bmap/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import HotelAccInfo
 # Register your models here.
 
-admin.site.register(HotelAccInfo)
+#admin.site.register(HotelAccInfo)
+
+class HotelAccInfoAdmin(admin.ModelAdmin):
+    list_display = ("name", "gender", "idcard", "birth",
+                    "city", "hotel", "room", "checkintime", "checkouttime")
+    list_filter = ("name", "city", "hotel")
+
+admin.site.register(HotelAccInfo, HotelAccInfoAdmin)
